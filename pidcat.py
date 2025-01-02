d160c45af620af9d426dabaeaf666923b4dca981
Emperor-1231 <abdoakojdad@gmail.com>
2025-01-02 16:00:48 +0100

    pidcat.py

--- pidcat.py
+++ pidcat.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python -u
+# مستنسخ من Manga Akojdad 
 
 '''
-Copyright 2009, The Android Open Source Project
+حقوق النشر 2009، The Android Open Source Project
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+مرخصة تحت رخصة Apache، الإصدار 2.0 (الرخصة)؛
+لا يمكنك استخدام هذا الملف إلا وفقًا للرخصة.
+يمكنك الحصول على نسخة من الرخصة على العنوان التالي:
 
   http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+ما لم يكن مطلوبًا بموجب القانون أو متفقًا عليه كتابيًا، يتم توزيع البرنامج
+على أساس "كما هو"، دون أي ضمانات أو شروط من أي نوع، سواء كانت صريحة أو ضمنية.
+انظر الرخصة للحصول على التفاصيل الخاصة باللغة والإذن.
 '''
 
 # Script to highlight adb logcat output for console
@@ -63,7 +62,7 @@ if (args.no_color):
 
 package = args.package
 if len(package) == 0:
-  package.append('com.rajarsheechatterjee.LNReader')
+  package.append('com.akojdad.novels')
 
 # enabling Windows 10 VT100 support
 def enableVT100():
